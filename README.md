35f4b40d35c8e81ec7f1d4f8726f1ee5e4f58d00
Eduardo Kipper <eduardo.k.rubio@gmail.com>
2023-11-18 18:36:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# CRUD em Python para Json
+
+### Objetivo:
+
+Esse projeto tem como objetivo a realização de um CRUD com a temática
+de um controle de procedimentos de um salão de beleza utilizando a
+linguagem de programação Python e guardando os dados criados em um
+arquivo ".json".
+
+### Funcionalidades:
+
+1 - O Usuário deve ter a possibilidade de criar um dado e salvar no
+arquivo ".json".
+
+2 - O Usuário deve conseguir ler o os dados no terminal que estão
+armazenados no arquivo ".json".
+
+3 - O Usuário deve poder atualizar um dado e salvar o dado atualizado.
+
+4 - O usuário deve ter a possibilidade de excluir um dado armazenado
+no arquivo.
+
+5 - O usuário poderá realizar um backup do arquivo existente.
+
+### Aplicação:
+
+para funcionar o código basta rodar o arquivo "main.py", com ele
+todas as funções serão abertas em sequência.
+
+### Equipe:
+
+Eduardo Kipper Della Santa Rubio
---
+++ function - Backup.py
@@ -0,0 +1,386 @@
+import json #Importa a biblioteca json
+import os #Importa a biblioteca os
+
+# Inicia o Código
+def iniciar():
+    exibir_menu()
+
+# Cria um dicionário chamado dados
+dados = {}
+
+# Cria um arquivo json se ele não for encontrado na pasta local
+if not os.path.isfile('procedimentos.json'):
+    arquivo_json = open('procedimentos.json', 'w')
+    json.dump(dados, arquivo_json)
+    arquivo_json.close()
+
+# Atualiza o dicionário "dados" com o arquivo json selecionado
+def atualiza_json():
+
+    arquivo_json = open('procedimentos.json', 'r')
+    tamanho_json = arquivo_json.read()
+    arquivo_json.close()
+
+    # Se o arquivo json possuir mais de um registro, a variavel dados recebe o valor do json
+    if len(tamanho_json) > 1:
+        global dados
+        arquivo_json = open('procedimentos.json', encoding="utf-8")
+        objeto_json = json.load(arquivo_json)
+        arquivo_json.close()
+        dados = objeto_json
+        return dados
+
+# Função que Imprime o menu de opções
+def exibir_menu():
+    while True:
+        atualiza_json() #Inicia a função atualiza_json
+        global dados
+        #Imprime o menu de opções
+        print(''' 
+<<<<<<<<<<<<<<<<Opções>>>>>>>>>>>>>>>>
+1 - Cadastrar Procedimento
+2 - Pesquisar Procedimento
+3 - Alterar Procedimento (ainda não implementado)
+4 - Excluir Procedimento (ainda não implementado)
+5 - Realizar Backup do Arquivo
+6 - Puxar Backup para o Arquivo
+0 - Sair
+<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>
+''')
+
+        # Pede para o Usuário escolher qual opção quer realizar
+        while True:
+            try:
+                escolha = int(input("Digite a opção escolhida: "))
+                break
+            except ValueError:
+                print("ERRO! Valor inválido, Digite um número inteiro")
+
+        # Realiza funções de acordo com a escolha do usuário
+        if escolha == 1: #Criar
+            print("\n<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>\n")
+            # Recebe o valor nome do procedimento que servirá como uma chave/identificador no dicionário
+            print("Digite o nome do procedimento")
+            nome_procedimento = (input("-----------:"))
+            print()
+            cadastrar_procedimento(nome_procedimento)
+            arquivo_json = open("procedimentos.json", "w", encoding="utf-8")
+            json.dump(dados, arquivo_json, ensure_ascii=False, indent=4)
+            arquivo_json.close()
+        elif escolha == 2: #Ler
+            print("\n<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>\n")
+            if len(dados) < 1:
+                print("Nenhum procedimento cadastrado ainda")
+            else:
+                while True:
+                    listar_procedimento()
+                    pesquisar_procedimento()
+                    print("\nDigite 'sim' para continuar a pesquisa")
+                    continuar = input("-----------:")
+                    print()
+                    if continuar.lower() != "sim" and continuar.lower() != "s":
+                        break
+        elif escolha == 3: #Atualizar
+            print("\n<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>\n")
+            if len(dados) < 1:
+                print("Nenhum procedimento cadastrado ainda")
+            else:
+                while True:
+                    listar_procedimento()
+                    alterar_procedimento()
+                    arquivo_json = open("procedimentos.json", "w", encoding="utf-8")
+                    json.dump(dados, arquivo_json, ensure_ascii=False, indent=4)
+                    arquivo_json.close()
+                    print("\nDigite 'sim' para continuar à alterar")
+                    continuar = input("-----------:")
+                    print()
+                    if continuar.lower() != "sim" and continuar.lower() != "s":
+                        break
+        elif escolha == 4: #Excluir
+            dados.pop("Corte Curt")
+            arquivo_json = open("procedimentos.json", "w", encoding="utf-8")
+            json.dump(dados, arquivo_json, ensure_ascii=False, indent=4)
+            arquivo_json.close()
+        elif escolha == 5: #Fazer Backup
+            print("\n<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>\n")
+            fazer_backup()
+            print("<<<<<Backup Realizado Com Sucesso>>>>>")
+        elif escolha == 6:
+            print("\n<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>\n")
+            try:
+                puxar_backup()
+                arquivo_json = open("procedimentos.json", "w", encoding="utf-8")
+                json.dump(dados, arquivo_json, ensure_ascii=False, indent=4)
+                arquivo_json.close()
+                print("<<<<Arquivo Atualizado Com Sucesso>>>>")
+            except:
+                print("Você não possui nenhum Arquivo de Backup")
+        elif escolha == 0: # Sair
+            print("\n<<<<<<<<<<<<<<Volte Logo>>>>>>>>>>>>>>")
+            break
+
+
+def cadastrar_procedimento(nome_procedimento):
+    global dados
+    # Cria um dicionário de Procedimentos
+    dict_procedimento = {}
+
+    # Cadastra a duração do procedimento
+    print("Digite a duração média do procedimento (min)")
+    while True:
+        try:
+            duracao_procedimento = int(input("-----------:"))
+            dict_procedimento["Duração"] = ("%d min"%duracao_procedimento)
+            break
+        except ValueError:
+            print("ERRO! Valor inválido, Digite um número inteiro")
+    print()
+
+    # Cadastrar valor do procedimento
+    print("Digite o valor do procedimento (moeda: R$)")
+    while True:
+        try:
+            valor = float(input("-----------:"))
+            dict_procedimento["Valor"] = ("R$%.2f"%valor)
+            break
+        except ValueError:
+            print("ERRO! Valor inválido, Digite um número real\n(separação dos decimais deve ser realizada com '.')")
+    print()
+
+    # Cadastrar materiais utilizados
+    print("Digite os materiais utilizados ('fim' para sair)")
+    materiais = []
+    while True:
+        material = input("-----------:")
+        if material.lower() == "fim" or material.lower() == "f" or material.lower() == "":
+            break
+        else:
+            materiais.append(material.title())
+    dict_procedimento["Materiais"] = materiais
+    print()
+
+    # Cadastrar os profissionais autorizados a realizar o procedimento
+    print("Digite quais os profissionais autrizados a\nrealizar o procedimento ('fim' para sair)")
+    profissionais = []
+    while True:
+        profissional = input("-----------:")
+        if profissional.lower() == "fim" or profissional.lower() == "f" or profissional.lower() == "":
+            break
+        else:
+            profissionais.append(profissional.title())
+    dict_procedimento["Profissionais"] = profissionais
+
+    #Adisciona os dados no dicionário dados
+    dados[nome_procedimento.title()] = dict_procedimento
+
+def listar_procedimento():
+    global dados
+    for chave, valor in dados.items():
+        print(chave)
+
+def pesquisar_procedimento():
+    global dados
+    print("\nQual procedimento deseja pesquisar?")
+    procedimento = input("-----------:")
+    chave_dados = dados.get(procedimento.title())
+    print()
+    try:
+        if chave_dados == dados[procedimento.title()]:
+            for chave, valor in chave_dados.items():
+                print(chave, "-", valor)
+    except KeyError:
+        print("Chave não encontrada")
+
+
+
+
+
+
+
+
+
+def alterar_procedimento():
+    global dados
+    print("\nQual procedimento deseja alterar?")
+    procedimento = input("-----------:")
+    chave_dados = dados.get(procedimento.title())
+    print()
+    try:
+        if chave_dados == dados[procedimento.title()]:
+            while True:
+                x = 1
+                for chave, valor in chave_dados.items():
+                    print(x, "-", chave, "-", valor)
+                    x += 1
+                print(9, "-", "Fim")
+
+
+
+                print("\nO que deseja alterar?")
+                alterar = input("-----------:")
+                match alterar.lower():
+
+                    case ("1"|"duração"|"duracao"|"duracão"|"duraçao"):
+                        print("Digite a duração média do procedimento (min)")
+                        while True:
+                            try:
+                                duracao_procedimento = int(input("-----------:"))
+                                chave_dados["Duração"] = ("%d min" % duracao_procedimento)
+                                break
+                            except ValueError:
+                                print("ERRO! Valor inválido, Digite um número inteiro")
+                        print()
+
+                    case ("2"|"valor"):
+                        print("Digite o valor do procedimento (moeda: R$)")
+                        while True:
+                            try:
+                                valor = float(input("-----------:"))
+                                chave_dados["Valor"] = ("R$%.2f" % valor)
+                                break
+                            except ValueError:
+                                print("ERRO! Valor inválido, Digite um número real\n(separação dos decimais deve ser realizada com '.')")
+                        print()
+
+                    case ("3"|"materiais"):
+                        print("Deseja Alterar, Remover ou Adicionar (Alt/Rem/Add)")
+                        adicionar = input("-----------:")
+
+                        if adicionar.lower() == "alt" or adicionar.lower() == "alterar":
+                            materiais = chave_dados["Materiais"]
+                            print()
+                            for valor in materiais:
+                                print(valor)
+                            print("Digite os materiais deseja alterar")
+                            material = input("-----------:")
+
+                            try:
+                                print("Digite o novo material ('fim' para sair):")
+                                novo_material = input("-----------:")
+                                materiais[materiais.index(material.title())] = novo_material.title()
+
+                            except ValueError:
+                                print("Valor não encontrado")
+
+                        elif adicionar.lower() == "rem" or adicionar.lower() == "remover":
+                            materiais = chave_dados["Materiais"]
+                            print()
+                            for valor in materiais:
+                                print(valor)
+                            print("Digite os materiais deseja remover ('fim' para sair)")
+                            while True:
+                                material = input("-----------:")
+                                if material.lower() == "fim" or material.lower() == "f" or material.lower() == "":
+                                    break
+                                else:
+                                    try:
+                                        materiais.pop(materiais.index(material.title()))
+                                    except ValueError:
+                                        print("Valor não encontrado")
+
+                        elif adicionar.lower() == "add" or adicionar.lower() == "adicionar":
+                            materiais = chave_dados["Materiais"]
+                            print("Digite os materiais utilizados ('fim' para sair)")
+                            while True:
+                                material = input("-----------:")
+                                if material.lower() == "fim" or material.lower() == "f" or material.lower() == "":
+                                    break
+                                else:
+                                    if materiais.index(material.title()):
+                                        print("Valor já existente")
+                                    else:
+                                        materiais.append(material.title())
+                            chave_dados["Materiais"] = materiais
+                            print()
+                        else:
+                            print("Opção inválida")
+
+                    case ("4"|"profissionais"):
+                        print("Deseja Alterar, Remover ou Adicionar (Alt/Rem/Add)")
+                        adicionar = input("-----------:")
+
+                        if adicionar.lower() == "alt" or adicionar.lower() == "alterar":
+                            profissionais = chave_dados["Profissionais"]
+                            print()
+                            for valor in profissionais:
+                                print(valor)
+                            print()
+                            print("Digite os profissionais que deseja alterar")
+                            profissional = input("-----------:")
+
+                            try:
+                                print("Digite o novo profissional ('fim' para sair):")
+                                novo_profissional = input("-----------:")
+                                profissionais[profissionais.index(profissional.title())] = novo_profissional.title()
+                            except ValueError:
+                                print("Valor não encontrado")
+
+                        elif adicionar.lower() == "rem" or adicionar.lower() == "remover":
+                            profissionais = chave_dados["Profissionais"]
+                            print()
+                            for valor in profissionais:
+                                print(valor)
+                            print()
+                            print("Digite os profissionais deseja remover ('fim' para sair)")
+                            while True:
+                                profissional = input("-----------:")
+                                if profissional.lower() == "fim" or profissional.lower() == "f" or profissional.lower() == "":
+                                    break
+                                else:
+                                    try:
+                                        profissionais.pop(profissionais.index(profissional.title()))
+                                    except ValueError:
+                                        print("Valor não encontrado")
+
+                        elif adicionar.lower() == "add" or adicionar.lower() == "adicionar":
+                            profissionais = chave_dados["Profissionais"]
+                            print("Digite os profissionais autorizados ('fim' para sair)")
+                            while True:
+                                profissional = input("-----------:")
+                                if profissional.lower() == "fim" or profissional.lower() == "f" or profissional.lower() == "":
+                                    break
+                                else:
+                                    if profissionais.index(profissional.title()):
+                                            print("Valor ja existente")
+                                    else:
+                                        profissionais.append(profissional.title())
+                            chave_dados["Profissionais"] = profissionais
+                            print()
+                        else:
+                            print("Opção inválida")
+
+                    case ("9" | "fim" | "f"):
+                        break
+
+    except KeyError:
+        print("Chave não encontrada")
+
+
+
+
+
+
+
+
+
+
+
+# Cria um novo documento de backup do arquivo existente
+def fazer_backup():
+    arquivo_json = open("procedimentos.json", encoding="utf-8")
+    carregamento_json = json.load(arquivo_json)
+    arquivo_json.close()
+    backup_json = open("backup_procedimentos.json", "w", encoding="utf-8")
+    json.dump(carregamento_json, backup_json, ensure_ascii=False, indent=4)
+
+#Puxa os valores do backup para o arquivo existente
+def puxar_backup():
+    global dados
+    backup_json = open("backup_procedimentos.json", encoding="utf-8")
+    carregamento_json = json.load(backup_json)
+    backup_json.close()
+    dados = carregamento_json
+    return dados
+
+
+
